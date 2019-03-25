bb6d90dd7cb18d6b98b6419cdfe5b11b581ba59d
Assel Seraliyeva <assel.seraliyeva@gmail.com>
2019-03-25 12:07:11 +0100

    Added index.js file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,45 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+$(document).ready(function () {
+  $('.responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1457,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('#clickMenu').click(() => {
+    let menu = document.getElementById("account");
+    menu.classList.toggle("dropdown-profile-content-show");
+  });
+});
+
+
